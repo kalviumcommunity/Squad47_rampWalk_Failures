0b80706891661c4f79b1c233c6684ad587f0b1b5
Kritika Walia <kritikawalia987@gmail.com>
2024-01-18 11:59:31 +0530

    Add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = process.env.PUBLIC_PORT ;
+
+// define the ping route with the response in JSON
+
+app.get('/ping', (req, res)=>{
+  res.json({message : "pong"});
+})
+
+if (require.main === module) {
+
+  app.listen(port, () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
+}
+
+module.exports = app;
