9dbab851264c6759bbdaf76e18c38a08a62b2386
Kritika Walia <kritikawalia987@gmail.com>
2024-02-01 14:38:53 +0530

    Addede entity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import Home from './components/Home'
+import Info from './components/Info'
 
 function App() {
 
   return (
     <>
       <Home/>
+      <Info/>
     </>
   )
 }
