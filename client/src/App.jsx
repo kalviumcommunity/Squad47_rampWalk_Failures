6b97a4eb9a9d98dc86bfe75704668d297cb468a4
Kritika Walia <kritikawalia987@gmail.com>
2024-02-03 10:43:25 +0530

    Connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import Home from './components/Home'
-import Info from './components/Info'
+import Info from './components/Info.jsx'
 
 function App() {
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <h1>Home</h1>
       <p>Some rampwalk failures will be shown in this website.</p>
       <div className="img"> image</div>
