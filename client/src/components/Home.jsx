8e94bdda0f2637cbc7b4e42109aa4430cbf29a78
Kritika Walia <kritikawalia987@gmail.com>
2024-02-15 14:37:13 +0530

    JWT Token

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
   const [data, setData] = useState([]);
